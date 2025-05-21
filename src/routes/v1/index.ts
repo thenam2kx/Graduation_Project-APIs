799f4363169b31e540d72c5335ec36b835bea2ae
Minq05 <hoangquan22022005@gmail.com>
2025-05-21 19:44:47 +0700

    apis crud cho module blog

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { userRoute } from './user.routes'
 import { authRoute } from './auth.routes'
+import { blogRoute } from './blog.routes'
 
 const Router = express.Router()
 
 Router.use('/users', userRoute)
+Router.use('/blogs', blogRoute)
 Router.use('/auth', authRoute)
 
 export const APIs_v1 = Router
